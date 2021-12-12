2e1cdd4b14deb3d458cf70033919691025fa5856
Pierre-William Lessard <lessardpw@gmail.com>
2021-12-11 21:20:00 -0500

    usable sentiment model function

---
+++ sentiment_model.py
@@ -0,0 +1,15 @@
+import os
+import spacy
+from spacy.tokenizer import Tokenizer
+
+def predict_sentiment(txt: str, direc: str = 'models/sentiment/saved_models/model50') -> float:
+    """
+    using model predict sentiment
+    input is a txt string
+    optional directory change for using different models
+    returns a value from -1 to 1
+    Aproaching -1 being a negative sentiment
+    Aproaching 1 being a positive sentiment
+    """
+    vals = spacy.load(direc)(txt).cats
+    return (f'{vals["pos"] if vals["pos"]>vals["neg"] else -1*vals["neg"]}')
