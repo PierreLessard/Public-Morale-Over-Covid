a909d4b778bfcdb1cdd14a7f12b9f5504d66254c
Pierre-William Lessard <lessardpw@gmail.com>
2021-12-12 15:24:43 -0500

    added more effecient functionality

--- sentiment_model.py
+++ sentiment_model.py
@@ -4,7 +4,9 @@ from spacy.tokenizer import Tokenizer
 
 def predict_sentiment(txt: str, direc: str = 'models/sentiment/saved_models/model50') -> float:
     """
-    using model predict sentiment
+    predicts sentiment of string
+    only use for testing not good for large data because
+    model is loaded each time
     input is a txt string
     optional directory change for using different models
     returns a value from -1 to 1
@@ -12,4 +14,22 @@ def predict_sentiment(txt: str, direc: str = 'models/sentiment/saved_models/mode
     Aproaching 1 being a positive sentiment
     """
     vals = spacy.load(direc)(txt).cats
+    return vals["pos"] if vals["pos"]>vals["neg"] else -1*vals["neg"]
+
+
+def open_model(direc: str = 'models/sentiment/saved_models/model50'):
+    """opens model from optional directory string"""
+    return spacy.load(direc)
+
+def model_predict_sentiment(model, txt: str) -> float:
+    """
+    use for larger data because model is not loaded each time
+
+    given a model input and string input,
+    output sentiment prediction
+    returns a value from -1 to 1
+    Aproaching -1 being a negative sentiment
+    Aproaching 1 being a positive sentiment
+    """
+    vals = model(txt).cats
     return vals["pos"] if vals["pos"]>vals["neg"] else -1*vals["neg"]
