7955b66bcb2517c03c5ab288dbd5ba02cbce8b2d
leowrites <siqi.liu.0922@gmail.com>
2021-12-13 17:59:37 -0500

    checked styles with python_ta

--- app.py
+++ app.py
@@ -251,21 +251,3 @@ def run_app() -> None:
     Runs the app
     """
     app.run_server(debug=True)
-
-
-
-if __name__ == '__main__':
-    import python_ta.contracts
-    python_ta.contracts.DEBUG_CONTRACTS = False
-    python_ta.contracts.check_all_contracts()
-
-    # import doctest
-    # doctest.testmod(verbose=True)
-
-    import python_ta
-    python_ta.check_all(config={
-        'extra-imports': ['time', 'util', 'datetime', 'python_ta.contracts', 'dash', 'pandas', 'os', 'pathlib', 'dash_bootstrap_components', 'plotly', 'plotly.express', 'dcc', 'html'],
-        'allowed-io': ['run_example'],
-        'max-line-length': 100,
-        'disable': ['R1705', 'C0200', 'R0201']
-})
