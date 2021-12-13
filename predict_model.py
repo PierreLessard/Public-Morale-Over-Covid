fa19ba1142b4e8e60292de9e4d39973759edba74
leowrites <siqi.liu.0922@gmail.com>
2021-12-13 18:06:35 -0500

    checked style with python_ta

--- predict_model.py
+++ predict_model.py
@@ -1,58 +1,59 @@
+"""Process the comments data to raw sentiment data, and process the raw sentiment data"""
 from os import listdir
 from os.path import isfile, join
-import models.sentiment_model as sentiment_model 
+import os
 import datetime
-import pandas as pd
-import logging
 import csv
 import multiprocessing as mp
+import pandas as pd
+from models import sentiment_model
 
 
-def predict_sentiment(files) -> None:
+def predict_sentiment(files: list[str]) -> None:
     """
     Use multiprocessing to speed up prediction process
     All files are passed in here and the model is applied.
     The output is a csv file written to data/prediction_outputs
+
+    Precondition:
+        - files are a list of valid paths to unprocessed sentiment files.
     """
     for file in files:
         date_and_value = []
-        logging.basicConfig(level=logging.DEBUG)
         data = pd.read_csv(f'data/comments/{file}')
         model = sentiment_model.open_model()
         for index in range(len(data['Comments'])):
             out = sentiment_model.model_predict_sentiment(model, data.loc[index, 'Comments'])
-            date_and_value.append([datetime.datetime.strptime(data.loc[index, 'Date'].split(' ')[0], '%Y-%m-%d'), out])
-            logging.debug(f"Running {index} on file at data/comments/{file} Sentiment: {out}")
+            date_and_value.append([datetime.datetime.strptime(
+                data.loc[index, 'Date'].split(' ')[0], '%Y-%m-%d'), out])
 
         # write file output
-        with open(f'data/prediction_outputs/predicted_sentiment{file.strip("comments")}', 'w', encoding='UTF8', newline='') as f:
+        with open(f'data/prediction_outputs/predicted_sentiment{file.strip("comments")}',
+                  'w', encoding='UTF8', newline='') as f:
             writer = csv.writer(f)
-            logging.debug(f"Writing to data/comments/{file}")
             writer.writerow(['Date', 'Sentiment'])
             writer.writerows(date_and_value)
-            logging.debug(f"Writing compelete")
 
 
-def start_processes(ps):
+def process_raw_sentiment(file: str = os.path.join('data',
+                          os.path.join('prediction_outputs',
+                          'predicted_sentiment_all_raw.csv'))) -> None:
+    """
+    Takes in a file containing all the data of sentiments and
+    calculate the average for each day, then write the date and
+    average sentiment to a new file
+    """
+    df = pd.read_csv(file)
+    n = df.groupby(['Date']).mean()
+    n.to_csv(os.path.join('data',
+                          os.path.join('prediction_outputs',
+                          'predicted_sentiment_all_processed.csv')))
+
+
+def start_processes(ps: list[mp.Process]) -> None:
     """Starts the Process object.
     """
     for p in ps:
         p.start()
     for p in ps:
         p.join()
-
-
-if __name__ == '__main__':
-    my_path = 'data/comments'
-    files = [f for f in listdir(my_path) if isfile(join(my_path, f))]
-    files_1 = files[:9]
-    files_2 = files[9 : 18]
-    files_3 = files[18: 27]
-    files_4 = files[27: 36]
-    files_5 = files[36: 42]
-    files_6 = files[42: 52]
-    l = [files_1, files_2, files_3, files_4, files_5, files_6]
-    # WARNNING: This file applys the model to the datasheets in 6 parrallel processes.
-    # It uses 6 cores and takes 100% of the CPU. 
-    # processes = [mp.Process(target=predict_sentiment, args=(file, )) for file in l]
-    # start_processes(processes)
